ba7773ef1fb8981c40e5e967b159865735ff1615
gotcha-d <gotcha.privatemail@gmail.com>
2025-07-29 22:25:51 +0900

    その他のケース追加

--- tests/Core/NumberConverterTest.php
+++ tests/Core/NumberConverterTest.php
@@ -14,6 +14,9 @@ class NumberConverterTest extends TestCase
         $this->assertEquals("Fizz", $fizzBuzz->convert(3));
         $this->assertEquals("4", $fizzBuzz->convert(4));
         $this->assertEquals("Buzz", $fizzBuzz->convert(5));
+        $this->assertEquals("Fizz", $fizzBuzz->convert(6));
+        $this->assertEquals("Buzz", $fizzBuzz->convert(10));
         $this->assertEquals("FizzBuzz", $fizzBuzz->convert(15));
+        $this->assertEquals("FizzBuzz", $fizzBuzz->convert(30));
     }
 }
