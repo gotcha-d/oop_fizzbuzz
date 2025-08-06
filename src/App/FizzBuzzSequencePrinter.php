37b725896c922ae3e89b90f8d6493cab25c07ed3
gotcha-d <gotcha.privatemail@gmail.com>
2025-08-07 08:53:51 +0900

    依存性の問題点についてコメント

--- src/App/FizzBuzzSequencePrinter.php
+++ src/App/FizzBuzzSequencePrinter.php
@@ -10,6 +10,8 @@ class FizzBuzzSequencePrinter
 {
     public function printRange(int $begin, int $end): void
     {
+        // 自力でNumberConverterオブジェクトを生成している
+        // 別のパッケージに強い依存が生まれていて、FizzBuzzSequencePrinter の単体テストのために具象レベルの依存が強いられる
         $fizzBuzz = new NumberConverter([
             new CyclicNumberRule(3, "Fizz"),
             new CyclicNumberRule(5, "Buzz"),
